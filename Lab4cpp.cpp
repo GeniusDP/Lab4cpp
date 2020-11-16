f0e9d870223bfc431421deb5438619a8693c4ad5
Zaranik Bogdan <zaranikBZ@gmail.com>
2020-11-16 10:13:36 +0200

    Добавьте файлы проекта.

---
+++ Lab4cpp.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+int main() {
+    //оглашение переменных и ввод
+    int n, i;
+    cin >> n;
+    double ans = 1;
+    //рабочий блок с выводом
+    //промежуточных результатов
+    for (i = 1; i <= n; i++) {
+        //домножение ответа на техущий х
+        ans *= (2 * i - 1) / (2 * i + 0.);
+        cout << "ans(" << setw(3) << i << ") = " << setw(10) << fixed << setprecision(8) << ans << endl;
+    }
+    //вывод ответа
+    cout << "Result is : " << ans << endl;
+    return 0;
+}
